f78101796317e2a92aa88330974582c018687cf6
Brian Marin <brian200218@gmail.com>
2024-05-19 17:46:48 -0500

    Cambios

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,12 @@ import TransaccionView from '../views/Transaccion.vue';
 import Proveedores from '../views/Proveedores.vue';
 import NewProveedores from '../components/Proveedor/NewProveedor.vue';
 import EditProveedores from '../components/Proveedor/EditProveedor.vue';
+import NewCategoria from '../components/Categoria/NewCategoria.vue';
+import EditCategoria from '../components/Categoria/EditCategoria.vue';
+import NewProducto from '../components/Producto/NewProducto.vue';
+import EditProducto from '../components/Producto/EditProducto.vue';
+import NewTransaccion from '../components/Transaccion/NewTransaccion.vue';
+import EditTransaccion from '../components/Transaccion/EditTransaccion.vue';
 import Inventarios from '../views/Inventarios.vue';
 
 const routes = [
@@ -29,6 +35,36 @@ const routes = [
     path: '/edit-proveedores/:id',
     name: 'EditProveedor',
     component: EditProveedores
+  },
+  {
+    path: '/new-categorias',
+    name: 'NewCategoria',
+    component: NewCategoria
+  },
+  {
+    path: '/edit-categorias/:id',
+    name: 'EditCategoria',
+    component: EditCategoria
+  },
+  {
+    path: '/new-productos',
+    name: 'NewProducto',
+    component: NewProducto
+  },
+  {
+    path: '/edit-productos/:id',
+    name: 'EditProducto',
+    component: EditProducto
+  },
+  {
+    path: '/new-transacciones',
+    name: 'NewTransacciones',
+    component: NewTransaccion
+  },
+  {
+    path: '/edit-transacciones/:id',
+    name: 'EditTransacciones',
+    component: EditTransaccion
   },
   {
     path: '/inventarios',
